0dfeaf43f0f450809df371d68261d40214fb3e32
Paul Martin <pdmart@gmail.com>
2014-08-11 11:45:22 -0700

    added basic client/server code

---
+++ Client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 31000)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
---
+++ StartServer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# ===== IMPORTS =====
+import SocketServer
+from threading import Thread
+import sys
+
+# ===== CLIENT HANDLER ===== 
+class ClientHandler(SocketServer.BaseRequestHandler):
+    
+    # function for handling new connections
+    def handle(self):
+        data = 'default'
+        print "Client connected from ", self.client_address
+        while len(data):
+            data = self.request.recv(1024)
+            # application-specific handling of data
+            self.request.send(data)
+
+        print "Client exited from ", self.client_address
+        self.request.close()
+
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+if len(sys.argv) < 2:
+    print 'Usage: python StartServer.py <port_num>'
+else:
+    print 'Opening Server on port', str(sys.argv[1])
+    ThreadedTCPServer(('',31000), ClientHandler).serve_forever()
